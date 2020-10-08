e903c8d7c1e1ab5d7e8bf97d4d99c1d3f83c3c4e
tavish <tavish.gobindram@gmail.com>
2020-10-08 20:25:38 +0800

    add function implementations

--- functions/index.js
+++ functions/index.js
@@ -15,8 +15,33 @@ app.get("/", (req, res) => {
   return res.status(200).send("Hello World!");
 });
 
-app.post("/write-doc", async (req, res) => {});
+app.post("/write-doc", async (req, res) => {
+  const { collection, documentId, documentValue } = req.body;
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.set(documentValue);
+    console.log("Write Success", r);
+    return res.status(200).end();
+  } catch (e) {
+    console.error("Write Failure", r);
+    return res.status(500).end();
+  }
+});
 
-app.get("/read-doc", async (req, res) => {});
+app.get("/read-doc", async (req, res) => {
+  const { collection, documentId } = req.query;
+  const docRef = db.collection(collection).doc(documentId);
+  try {
+    const r = await docRef.get();
+    console.log("Read Success", r);
+    return res.status(200).send({
+      name: r.id,
+      value: r.data(),
+    });
+  } catch (e) {
+    console.error("Read Failure", e);
+    return res.status(500).end();
+  }
+});
 
 exports.api = functions.https.onRequest(app);
